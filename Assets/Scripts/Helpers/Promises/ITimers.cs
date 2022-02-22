e20f6f4f9471fe87e3afbd33fc0e89b4b7fa8b3c
michael <you@example.com>
2022-02-22 15:07:11 +0300

    init

---
+++ Assets/Scripts/Helpers/Promises/ITimers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Tactics.Helpers.Promises
+{
+    public interface ITimers
+    {
+        float GetTime();
+        float GetTimeUnscaled();
+        IPromise WaitOneFrame();
+        IPromise Wait(float seconds, Action<float> progressCallback = null);
+        IPromise WaitUnscaled(float seconds, Action<float> progressCallback = null);
+        IPromise WaitForTrue(Func<bool> condition);
+    }
+}
---
+++ Assets/Scripts/Root.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Tactics
+{
+    public class Root : MonoBehaviour
+    {
+        private static Root _instance;
+
+        void Awake()
+        {
+            _instance = this;
+            DG.Tweening.DOTween.SetTweensCapacity(500, 100);
+        }
+
+        void Start()
+        {
+            print(gameObject.name);
+        }
+    }
+}
